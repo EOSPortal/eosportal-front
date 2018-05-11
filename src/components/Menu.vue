ad0073a50dc99ff0e5eff51b14873cf26d1c578a
Asgeir Sognefest <sogasg@gmail.com>
2018-05-11 16:08:26 +0200

    Add menu link to producers

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,6 +1,7 @@
 <template>
     <nav class="menu">
         <router-link to="/">Home</router-link>
+        <router-link to="/producers">Producers</router-link>
         <router-link to="/blocks">Blocks</router-link>
         <router-link to="/about">About</router-link>
     </nav>
