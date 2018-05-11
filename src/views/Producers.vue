9f43a76a09a1300e7a23f1ac3a6da92b376e6942
Asgeir Sognefest <sogasg@gmail.com>
2018-05-11 16:08:11 +0200

    List the producers in the producer view

--- src/views/Producers.vue
+++ src/views/Producers.vue
@@ -1,18 +1,41 @@
 <template>
     <div class="producers">
-
+        Chain: {{chainName}}
+        <table>
+            <tr v-bind:key="producer.id" v-for="producer in producers">
+                <td>{{producer.name}}</td>
+                <td>{{producer.organization}}</td>
+                <td>{{producer.contact}}</td>
+                <td>{{producer.location}}</td>
+                <td>{{producer.p2p_url}}</td>
+                <td>{{producer.api_url}}</td>
+                <td>{{producer.organization}}</td>
+                <td>{{producer.organization}}</td>
+            </tr>
+        </table>
     </div>
 </template>
 
 <script lang="ts">
-    import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue } from "vue-property-decorator";
+import { mapState, mapActions } from "vuex";
+import { getAllProducers } from "../api";
 
-    @Component({
-        components: {
+@Component({
+  components: {},
+  props: {},
+  computed: mapState(["chainName", "producers"]),
+  methods: mapActions(["getProducers"])
+})
+export default class Producers extends Vue {
+  producers: Array<any>;
+  chainName: string;
+  getProducers: () => void;
 
-        },
-    })
-    export default class Producers extends Vue {}
+  created() {
+    this.getProducers();
+  }
+}
 </script>
 
 <style lang="scss">
