8ad2d5875aefe13279486ae7f8407e09fe252d4a
Asgeir Sognefest <sogasg@gmail.com>
2018-05-11 16:07:33 +0200

    Add producers to the store
    This is a naive implementation not taking into account pagination, error handling etc.

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,25 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
+import { getAllProducers } from "./api";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    chainName: "prototype",
+    producers: []
   },
   mutations: {
-
+    setChain(state: any, chainName: string) {
+      state.chainName = chainName;
+    },
+    setProducers(state: any, producers: Array<any>) {
+      state.producers = producers;
+    }
   },
   actions: {
-
+    async getProducers({ commit, state }) {
+      commit("setProducers", (await getAllProducers(state.chainName)).data);
+    }
   }
-})
+});
