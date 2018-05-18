02e1eb52fe6f1a42033fe6198d90066dc44ae6a0
Calexxxxx <pasm28@hotmail.nl>
2018-05-18 12:19:13 +0200

    Add: some basic info for router-view test

--- src/views/Infos.vue
+++ src/views/Infos.vue
@@ -1,16 +1,18 @@
 <template>
-    <div class="infos">
-
+    <div class="container">
+        <h1 class="text-center p-4 m-4">{{chainName}} Chain Information</h1>
     </div>
 </template>
 
 <script lang="ts">
     import { Component, Vue } from 'vue-property-decorator';
+    import { mapState } from "vuex";
 
     @Component({
         components: {
 
         },
+        computed: mapState(["chainName"]),
     })
     export default class Infos extends Vue {}
 </script>
