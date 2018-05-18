04680559bd9ea96d5e538c0ca3ac3958466ed8f6
Calexxxxx <pasm28@hotmail.nl>
2018-05-18 12:19:59 +0200

    Add: Chain specific navigation component

---
+++ src/components/ChainNavigation.vue
@@ -0,0 +1,31 @@
+<template>
+    <b-nav pills class="d-flex justify-content-center p-2 m-2">
+        <b-nav-item exact-active-class="active" :to="{ name: 'infos' }" exact>Chain Info</b-nav-item>
+        <b-nav-item exact-active-class="active" :to="{ name: 'blocks' }">Blocks</b-nav-item>
+        <b-nav-item exact-active-class="active" :to="{ name: 'producers' }">Producers</b-nav-item>
+    </b-nav>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from "vue-property-decorator";
+    /**
+     * Bootstrap-vue components
+     */
+    import bNav from 'bootstrap-vue/es/components/nav/nav';
+    import bNavItem from 'bootstrap-vue/es/components/nav/nav-item';
+
+    @Component({
+        components: {
+            bNav,
+            bNavItem
+        }
+    })
+
+    export default class ChainNavigation extends Vue {
+
+    }
+</script>
+
+<style lang="scss">
+
+</style>
