f31c22622651df49be40f2850952405c3dda42ff
Shai James <skyandbeyond@gmail.com>
2018-05-26 01:30:56 +0300

    api catch fix

--- src/api.ts
+++ src/api.ts
@@ -5,6 +5,6 @@ import {
 } from "./utils/api.util";
 
 export const getChains = () => fetchJson(`${baseUrl}/chains`).then((res:any) => res.data).catch(() => []);
-export const getChain = (chain_id:string) => fetchJson(`${baseUrl}/chain/${chain_id}`).then((res:any) => res.data).then(() => null);
+export const getChain = (chain_id:string) => fetchJson(`${baseUrl}/chain/${chain_id}`).then((res:any) => res.data).catch(() => null);
 export const addChain = (url:string) => postJson(`${baseUrl}/chain`, {url});
 
