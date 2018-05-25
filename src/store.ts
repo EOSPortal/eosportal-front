02537c8dfbd593fdd401fab3953eac2092c2943b
Asgeir Sognefest <sogasg@gmail.com>
2018-05-25 13:07:48 +0200

    Revert "Cleanup in the sore and remove chainId from the store"
    This reverts commit d561a292589a6a03bfe339dc33c4ed4ed0853fa2.

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import { getChains} from "./api";
+import { getAllProducers , getChains} from "./api";
 import * as urlUtils from "@/utils/url.util";
 
 import Eos from 'eosjs';
@@ -9,6 +9,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    chainId: "",
     chains: [],
     chainData:null,
     theme:'dark',
@@ -22,8 +23,11 @@ export default new Vuex.Store({
 
 
   mutations: {
-    setTheme(state:any, theme:string) {
+    async setTheme(state:any, theme:string) {
       state.theme = theme;
+    },
+    setChain(state: any, chainId: number) {
+      state.chainId = chainId;
     },
     setChainData(state:any, chainData:any){
       state.chainData = chainData;
@@ -47,19 +51,22 @@ export default new Vuex.Store({
 
 
   actions: {
-    setTheme({ commit }, theme:string) {
+    async setTheme({ commit }, theme:string) {
       commit("setTheme", theme);
+    },
+    async setChain({ commit, state }, chain:any) {
+      commit("setChain", chain);
     },
     async getChains({ commit, state }) {
       commit("setChains", await getChains());
     },
-    setChainData({ commit, state }, chainData:any) {
+    async setChainData({ commit, state }, chainData:any) {
       commit("setChainData", chainData);
     },
-    setScatter({ commit, state }, scatter:any) {
+    async setScatter({ commit, state }, scatter:any) {
       commit("setScatter", scatter);
     },
-    setProducers({ commit, state }, producers:any[]) {
+    async setProducers({ commit, state }, producers:any[]) {
       commit("setProducers", producers);
     },
 
@@ -70,12 +77,12 @@ export default new Vuex.Store({
       const {host, port} = urlUtils.urlToHostPort(networkString);
       commit('setNetwork', { blockchain:'eos', host, port});
     },
-    login({state}){
+    async login({state}){
       if(!state.scatter) return false;
       if(!state.network) return false;
       return state.scatter.getIdentity({accounts:[state.network]});
     },
-    logout({state}){
+    async logout({state}){
       if(!state.scatter) return false;
       if(!state.scatter.identity) return false;
       return state.scatter.forgetIdentity();
