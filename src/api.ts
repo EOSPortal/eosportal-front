cc5828c4a1d26be6aa376200beac95a19de158fc
Asgeir Sognefest <sogasg@gmail.com>
2018-05-25 20:38:38 +0200

    Bug fix

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,7 @@ import {
   postJson
 } from "./utils/api.util";
 
-export const getChains = () => fetchJson(`${baseUrl}/chains`).then((res:any) => res.data).then(() => []);
+export const getChains = () => fetchJson(`${baseUrl}/chains`).then((res:any) => res.data).catch(() => []);
 export const getChain = (chain_id:string) => fetchJson(`${baseUrl}/chain/${chain_id}`).then((res:any) => res.data).then(() => null);
 export const addChain = (url:string) => postJson(`${baseUrl}/chain`, {url});
 
