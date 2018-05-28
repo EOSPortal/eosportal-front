47a1e67221a580ce504834c1b2149a9e37dd0d12
Cesar Rodriguez <kesarr@gmail.com>
2018-05-28 10:31:10 +0200

    heroku deploy

---
+++ dist/precache-manifest.8b855031d571c2578792151634f604c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb672539b8f713bb63de",
+    "url": "/js/vendors~app.cb672539.js"
+  },
+  {
+    "revision": "e6548aef4284cbd600bb",
+    "url": "/js/app.e6548aef.js"
+  },
+  {
+    "revision": "279b0e7f645b5e0e6b8bee1cd7064ba5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d88458f06cc78805a2cd339a4141013",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "e6548aef4284cbd600bb",
+    "url": "/css/app.7426c983.css"
+  }
+];
