2ce9cad10edfd56670cdac74400525b9beb6dc88
Asgeir Sognefest <sogasg@gmail.com>
2018-05-29 15:02:22 +0200

    List information from the block producers bp.json file

--- src/utils/api.util.ts
+++ src/utils/api.util.ts
@@ -3,11 +3,16 @@ import config from "../config";
 export const baseUrl = `https://${config.domain}`;
 
 export const fetchJson = (url: string) =>
-    fetch(url)
-        .then((res: any) => res.json()) 
-        //.catch(error => console.error(error)); // can we let this fail (let the errors bubble up) so we can catch the errors where it is used?
+  fetch(url).then((res: any) => res.json());
 
 export const postChain = (url: string, chain: string) =>
-    fetch(url, { method: 'POST', body: "url=" + chain, headers:{ 'Content-Type': 'application/x-www-form-urlencoded' } })
-        .then((res:any) => res.json())
-        .catch((error:any) => console.error(error));
+  fetch(url, {
+    method: "POST",
+    body: "url=" + chain,
+    headers: { "Content-Type": "application/x-www-form-urlencoded" }
+  })
+    .then((res: any) => res.json())
+    .catch((error: any) => console.error(error));
+
+export const getBpStandardInfo = (bpUrl: string) =>
+  fetch(`//${config.domain}/bps/${bpUrl}`).then((res: any) => res.json());
