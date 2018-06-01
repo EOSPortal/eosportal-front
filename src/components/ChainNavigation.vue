8b0e96a123ffedc1781f5c80620d620313551173
Shai James <skyandbeyond@gmail.com>
2018-06-01 07:52:42 +0300

    help

--- src/components/ChainNavigation.vue
+++ src/components/ChainNavigation.vue
@@ -1,7 +1,7 @@
 <template>
     <section>
         <section class="contain chain-nav">
-            <router-link exact-active-class="active" tag="button" :to="{ name: 'power' }">{{ $t('lang.votingPower') }}</router-link>
+            <!--<router-link exact-active-class="active" tag="button" :to="{ name: 'power' }">{{ $t('lang.votingPower') }}</router-link>-->
             <router-link exact-active-class="active" tag="button" :to="{ name: 'producers' }">{{ $t('lang.producers') }}</router-link>
             <router-link exact-active-class="active" tag="button" :class="{'full':!canShowScatterButton()}" :to="{ name: 'info' }" exact>{{ $t('lang.chainInfo') }}</router-link>
             <button v-if="canShowScatterButton()" @click="loginWithScatter">{{ suggestingNetwork ? 'Use Chain' : $t('lang.scatter') }}</button>
