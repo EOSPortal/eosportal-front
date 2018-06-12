899caafc642f73de19f40921f8360e668e78bd82
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2018-06-12 14:09:39 +0200

    adding src/components/Footer.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-
     <div id="app" :class="theme">
         <section class="app-container">
             <Menu></Menu>
             <router-view class="content"/>
+            <Footer></Footer>
         </section>
     </div>
 </template>
@@ -11,11 +11,13 @@
 <script lang="ts">
     import { Component, Vue } from 'vue-property-decorator';
     import Menu from './components/Menu.vue';
+    import Footer from './components/Footer.vue';
     import {mapState} from "vuex";
 
     @Component({
         components: {
-            Menu
+            Menu,
+            Footer
         },
         computed: mapState(["theme"]),
     })
